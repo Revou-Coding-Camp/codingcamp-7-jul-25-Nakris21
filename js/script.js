a84963903519cc0d8220fae6249e25a03f6b1d81
unknown <khrisna.chanel@gmail.com>
2025-07-10 20:53:42 +0700

    latihan js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log ('javascript is running');
+//global list
+
+let tasks = [];
+
+//function to add a new task
+function addTask() {
+const taskInput = document.getElementById('task-input');
+const dueDateInput = document.getElementById('due-date-input');
+}
+
+function deleteTask() {
+    // code to delete a task
+}
+
+function filterTasks() {
+    // code to filter tasks based on status (completed, pending, etc.)
+}
